0a8ca505d0ae8a48d0e6f1496bde518b0a22d3b7
izzyjosh <calebayuba222@gmail.com>
2023-03-05 22:49:46 +0100

    made a change to my app

--- post/myapp/forms.py
+++ post/myapp/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Posts
+from .models import Posts, Comment
 
 class PostForm(forms.ModelForm):
 	class Meta:
 		model = Posts
 		fields = ("post", )
+		
+class CommentForm(forms.ModelForm):
+	class Meta:
+		model = Comment
+		fields = ("comment", )
